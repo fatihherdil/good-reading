fef9c731ac26ed8dff44bf78b9a3ae1e0c844f91
Fatih Erdil <mehmett.erdil@gmail.com>
2022-02-18 20:08:02 +0300

    Get Customer Orders Query & Validation Implemented

---
+++ src/GoodReading.Application/CustomerOrder/Queries/GetCustomerOrders/GetCustomerOrdersQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace GoodReading.Application.CustomerOrder.Queries.GetCustomerOrders
+{
+    public class GetCustomerOrdersQuery : IRequest<List<Domain.Entities.CustomerOrder>>
+    {
+        public string CustomerId { get; set; }
+    }
+}
---
+++ src/GoodReading.Application/CustomerOrder/Queries/GetCustomerOrders/GetCustomerOrdersQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using GoodReading.Application.Customer.Commands;
+using GoodReading.Domain.Exceptions;
+using GoodReading.Domain.Repositories;
+using MediatR;
+
+namespace GoodReading.Application.CustomerOrder.Queries.GetCustomerOrders
+{
+    public class GetCustomerOrdersQueryHandler : IRequestHandler<GetCustomerOrdersQuery, List<Domain.Entities.CustomerOrder>>
+    {
+        private readonly ICustomerOrderRepository _customerOrderRepository;
+
+        public GetCustomerOrdersQueryHandler(ICustomerOrderRepository customerOrderRepository)
+        {
+            _customerOrderRepository = customerOrderRepository;
+        }
+
+        public async Task<List<Domain.Entities.CustomerOrder>> Handle(GetCustomerOrdersQuery request, CancellationToken cancellationToken)
+        {
+            var validator = new GetCustomerOrdersQueryValidator();
+            var result = await validator.ValidateAsync(request, cancellationToken);
+            if (!result.IsValid)
+            {
+                var message = string.Join(",", result.Errors.Select(e => e.ErrorMessage));
+                throw new ApiException((int)HttpStatusCode.BadRequest, message);
+            }
+
+            var customerOrders = await _customerOrderRepository.GetCustomerOrdersAsync(request.CustomerId);
+
+            return customerOrders;
+        }
+    }
+}
